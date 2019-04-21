42c45f7e42cc2dcc033b61a1b6f5cfe35f6063de
kzr <2392737795@qq.com>
2019-04-22 01:51:52 +0800

    day4_22

--- src/jisuanqi4_17/index.js
+++ src/jisuanqi4_17/index.js
@@ -1,38 +1,132 @@
-function init() {
+
+function init(){
     var num=document.getElementById("num");
     num.value=0;
     num.disabled="disabled";
+    // var n1=document.getElementById("n1");
+    // n1.onclick=function(){
+    // 	alert("123");
+    // }
     var oButton=document.getElementsByTagName("input");
+    var btn_num1;
+    var fh;
     for(var i=0;i<oButton.length;i++){
-        oButton[i].onclick=function () {
-            if(!isNumber(this.value)){
-                num.value=(num.value+this.value)*1;
-            }else {
-                num.value=num.value
+        oButton[i].onclick=function(){
+            if(isNumber(this.value)){
+                //num.value=(num.value+this.value)*1;
+                if(isNull(num.value)){
+                    num.value=this.value;
+                }else{
+                    num.value=num.value+this.value;
+                }
+            }else{
+                //alert("fei");
+                var btn_num=this.value;
+
+                switch(btn_num){
+                    case "+":
+                        btn_num1=Number(num.value);
+                        num.value=0;
+                        fh="+";
+                        break;
+                    case "-":
+                        btn_num1=Number(num.value);
+                        num.value=0;
+                        fh="-";
+                        break;
+                    case "*":
+                        btn_num1=Number(num.value);
+                        num.value=0;
+                        fh="*";
+                        break;
+                    case "/":
+                        btn_num1=Number(num.value);
+                        num.value=0;
+                        fh="/";
+                        break;
+                    case ".":
+                        num.value=dec_number(num.value);
+                        break;
+                    case "←":
+                        num.value=back(num.value);
+                        break;
+                    case "C":
+                        num.value="0";
+                        break;
+                    case "+/-":
+                        num.value=sign(num.value);
+                        break;
+                    case "=":
+                        switch(fh){
+                            case "+":
+                                num.value=btn_num1+Number(num.value);
+                                //console.log(btn_num1);
+                                break;
+                            case "-":
+                                num.value=btn_num1-Number(num.value);
+                                //console.log(btn_num1);
+                                break;
+                            case "*":
+                                num.value=btn_num1*Number(num.value);
+                                //console.log(btn_num1);
+                                break;
+                            case "/":
+                                if(Number(num.value)==0){
+                                    alert("除数不能是0");
+                                    num.value=0;
+                                }else{
+                                    num.value=btn_num1/Number(num.value);
+                                }
+
+                                //console.log(btn_num1);
+                                break;
+                        }
+                        break;
+                }
             }
         }
+    }
+}
+/*正负号*/
+function sign(n){
+    // if(n.indexOf("-")==-1){
+    // 	  n="-"+n;
+    // }else{
+    // 	  n=n.substr(1,n.length);
+    // }
+    n=Number(n)*-1;
+    return n;
+}
+/*退位键*/
+function back(n){
+    n=n.substr(0,n.length-1);
+    if(isNull(n)){
+        n="0";
+    }
+    return n;
+}
+/*小数点*/
+function dec_number(n){
+    if(n.indexOf(".")==-1){
+        n=n+".";
+    }
+    return n;
+}
+/*验证文本框是否为空或者0*/
+function isNull(n){
+    if(n=="0" || n.length==0){
+        return true;
     }else{
-        
+        return false;
     }
+}
 
+function isNumber(n){
+    // if(!isNaN(n)){
+    //   	return true;//参数n是数字
+    // }else{
+    // 	   return false;//参数n不是数字
+    // }
+    return  !isNaN(n);
 }
-    function  isNull(n) {
-        if(n*1==0||n.length==0){
-            return true;
-        }else {
-            return false;
-        }
-    }
-function isNumber(n) {
-    return !isNaN(n);
-}
-// function num_1_click() {
-//     var num=document.getElementById("num")
-//      var n=num.value;
-//     if(n=="0"){
-//         n="1";
-//     }else {
-//         n=n+"1";
-//     }
-//     document.getElementById("num").value=n;
-// }
+// isNaN：不能转换为数字：true，可以转换成数字是false
---
+++ src/jisuanqi4_17/mooc.js
@@ -0,0 +1,8 @@
+/*
+  实现M键的作用
+ */
+function f() {
+    document.getElementById("button20").onclick=function () {
+        window.location.href="http://www.imooc.com";//弹出新的窗口
+    }
+}
