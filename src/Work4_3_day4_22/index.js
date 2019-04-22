8ac143be8e6dba9780b219679d5122e910e6af57
kzr <2392737795@qq.com>
2019-04-23 03:14:12 +0800

    day423

---
+++ src/Work4_3_day4_22/index.js
@@ -0,0 +1,10 @@
+
+function tijiao() {
+    var anniu = document.getElementById("anniu");
+    anniu.style.backgroundColor = "#07cbc9";
+}
+function f() {
+         var anniu=document.getElementById("anniu");
+         anniu.style.backgroundColor="gray";
+     }
+
